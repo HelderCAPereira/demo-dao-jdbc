96d1616888e057339622aa4a76ebdff07934155e
Helder Pereira <helder.cleber@gmail.com>
2019-05-15 21:50:17 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -16,8 +16,16 @@ public class DB {
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
-				Properties props = loadProperties();
+                                //loadProperties - método que carrega um arquivo de configurações
+                                //para acesso ao banco de dados
+				Properties props = loadProperties(); 
+                                //Do arquivo lido via loadProperties, lê uma linha no aquivo que
+                                //se inicia com dburl, o valor após essa palavra é colocada na String
+                                //url
 				String url = props.getProperty("dburl");
+                                
+                                //conecta-se a um banco de dados, informando o endereço
+                                //e o arquivo onde contém os arquivos com o usuário e senha
 				conn = DriverManager.getConnection(url, props);
 			}
 			catch (SQLException e) {
@@ -38,9 +46,17 @@ public class DB {
 	}
 	
 	private static Properties loadProperties() {
+            /*
+            Um arquivo de proprieades é uma ótima opção para passar configurações
+            para uma determinada aplicação que necessita de configurações externas
+            e a mesma em si não pode ser alterada. Um exemplo de aplicação seria
+            para conectar a um banco de dados.
+            Um arquivo de propriedade é nada mais do que um arquivo simples com 
+            extensão .properties
+            */
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			Properties props = new Properties();
-			props.load(fs);
+			Properties props = new Properties(); //cria um objeto do tipo Properties
+			props.load(fs); //carrega o aquivo de propriedades na variável props
 			return props;
 		}
 		catch (IOException e) {
