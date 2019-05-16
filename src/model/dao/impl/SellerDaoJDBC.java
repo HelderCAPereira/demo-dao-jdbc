4f952ecf15e89a2a344d3ebca56a15ba715fb231
Helder Pereira <helder.cleber@gmail.com>
2019-05-16 11:31:01 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -94,9 +94,87 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public List<Seller> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null; // isso é um framework
+        ResultSet rs = null; //isso é um framework
+        
+        try{
+            /*
+            Inserção de um comando sql
+            No caso estamos selecinando todos os campos da tabela seller e o campo
+            Name da tabela department que na nova tabela será chamada DepName
+            A ligação entre as tabelas seller e department será via o campo Id
+            Estou gerando uma nova tabela contendo todos os campos do Seller mais 
+            o campo Name da tabela department
+            */
+            st = conn.prepareStatement(
+                    "Select seller.*, department.Name as DepName "
+                    +"from seller inner join department "
+                    +"on seller.Department = department.Id "
+                    + "order by name");
+
+            //Esse resultado vem para mim na forma de tabela e preciso
+            //representá-lo na forma de objeto
+            rs = st.executeQuery();
+            
+            /*
+            List é uma inteface e a classe ArrayList está implementando essa interface List
+            List aceita elementos duplicados
+            Cada nó da lista possui o endereço para acessar o próximo nó
+            Os tipos de dados contidos na lista tem que ser iguais
+            */
+            List <Seller> list = new ArrayList<>();
+            
+            /*
+            Map é uma coleção de pares chave/valor, ou seja, todo valor inserido na lista
+            tem que inserir uma cahve associada a ele
+            Não se admite repetições do objeto chave;
+            */
+            Map<Integer, Department> map = new HashMap<>();
+            
+
+            /*
+            Verifica se existe elementos resultantes da execução do sql, 
+            se existe ele captura a primeira linha da tabela e retorna true
+            */
+            while(rs.next()){
+                
+                /*
+                procura um objeto department dentro do map, baseado na chave Department
+                que é um campo com valor inteiro, se existir é gerado um objeto
+                do tipo department e atribuído a dep, caso contrário retorna null
+                Faça isso para não ter de criar vários objeto do tipo Department na
+                memória
+                */
+                Department dep = map.get(rs.getInt("Department"));
+                if(dep == null){
+                    
+                    //chama um método para criar o objeto Department 
+                    dep = instantiateDepartment(rs);
+                    /*
+                    Inser o objeto dep no Map, sendo que a chave é o 
+                    indice Department, campo da classe department
+                    */
+                    map.put(rs.getInt("Department"), dep);
+                }
+                
+                //chama um objeto que cria o objeto Seller
+                Seller obj = instantiateSeller(rs, dep);
+                
+                //adiciona o objeto obj na lista
+                list.add(obj);    
+            }
+            return list;
+            
+        }catch(SQLException e){
+            throw new DbException(e.getMessage()); //fazendo minha mensagem personalizada
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        } 
+
     }
 
+    //criando o objeto Department
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
         Department dep = new Department();
         dep.setId(rs.getInt("Department"));
@@ -105,6 +183,7 @@ public class SellerDaoJDBC implements SellerDao{
                 
     }
 
+    //Método para criar o objeto Seller
     private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
         Seller obj = new Seller();
         obj.setId(rs.getInt("Id"));
@@ -118,18 +197,22 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public List<Seller> findByDepartment(Department department) {
-       PreparedStatement st = null; // isso é um framework
+        PreparedStatement st = null; // isso é um framework
         ResultSet rs = null; //isso é um framework
         
         try{
             /*
             Inserção de um comando sql
             No caso estamos selecinando todos os campos da tabela seller e o campo
-            Name da tabela department que na nova tabela será chamada gerada DepName
-            A ligação entre as tableas seller e department será via o campo Id
+            Name da tabela department que na nova tabela será chamada DepName
+            A ligação entre as tabelas seller e department será via o campo Id
             No caso a linha que quero obter é aquela que estou informando como 
             uma ?
-            st.getInt(1,id) - estou falando que o ? vale 1
+            st.getInt(1,department.getId()) - estou falando que o ? vale o retorno
+            do método department.getId();
+            Na prática é obtido uma tabela com os dados da tabelas seller  mais 
+            o campo Name da tabela department, no qual o campo seller.Department
+            for igua ao valor informado.
             */
             st = conn.prepareStatement(
                     "Select seller.*, department.Name as DepName "
@@ -142,21 +225,52 @@ public class SellerDaoJDBC implements SellerDao{
             //Esse resultado vem para mim na forma de tabela e preciso
             //representá-lo na forma de objeto
             rs = st.executeQuery();
+            
+            /*
+            List é uma inteface e a classe ArrayList está implementando essa interface List
+            List aceita elementos duplicados
+            Cada nó da lista possui o endereço para acessar o próximo nó
+            Os tipos de dados contidos na lista tem que ser iguais
+            */
             List <Seller> list = new ArrayList<>();
+            
+            /*
+            Map é uma coleção de pares chave/valor, ou seja, todo valor inserido na lista
+            tem que inserir uma cahve associada a ele
+            Não se admite repetições do objeto chave;
+            */
             Map<Integer, Department> map = new HashMap<>();
             
 
-            //uso esse if para ver se veio algum dado da minha consulta
+            /*
+            Verifica se existe elementos resultantes da execução do sql, 
+            se existe ele captura a primeira linha da tabela e retorna true
+            */
             while(rs.next()){
-                //Havendo dados retornados eu estou criando um objeto
-                //e atribuindo dados para o objeto
+                
+                /*
+                procura um objeto department dentro do map, baseado na chave Department
+                que é um campo com valor inteiro, se existir é gerado um objeto
+                do tipo department e atribuído a dep, caso contrário retorna null
+                Faça isso para não ter de criar vários objeto do tipo Department na
+                memória
+                */
                 Department dep = map.get(rs.getInt("Department"));
                 if(dep == null){
+                    
+                    //chama um método para criar o objeto Department 
                     dep = instantiateDepartment(rs);
+                    /*
+                    Inser o objeto dep no Map, sendo que a chave é o 
+                    indice Department, campo da classe department
+                    */
                     map.put(rs.getInt("Department"), dep);
                 }
                 
+                //chama um objeto que cria o objeto Seller
                 Seller obj = instantiateSeller(rs, dep);
+                
+                //adiciona o objeto obj na lista
                 list.add(obj);    
             }
             return list;
